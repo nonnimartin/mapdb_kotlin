d2a3acafde28b3e2842aad7e2004533035ed1862
nonnimartin <nonnimartin@gmail.com>
2020-02-07 14:38:02 -0800

    Able to read from Crawldb

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import org.mapdb.*;
+
+import java.lang.reflect.Array;
 import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        DB db = DBMaker.fileDB("data.db").make();
-        ConcurrentMap map = db.hashMap("map").createOrOpen();
-        System.out.println(map.toString());
+        DB db = DBMaker.fileDB("data.db").checksumHeaderBypass().make();
+        //System.out.println("Database names = " + db.getAllNames().toString());
+        //System.out.println("Deleted map = " + db.get("DELETED_MAP"));
+        HTreeMap deletedMap = db.get("infoMap");
+        System.out.println(deletedMap.getKeys());
+        Long fetchedItems = (Long)deletedMap.get("fetchedItems");
+        
+
     }
 }
