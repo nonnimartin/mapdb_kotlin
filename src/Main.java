205b597b342d6604ac68783e8a3a9e4615c17076
nonnimartin <nonnimartin@gmail.com>
2020-02-07 14:59:30 -0800

    Able to read from Crawldb

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,23 @@ import java.util.concurrent.*;
 public class Main {
 
     public static void main(String[] args) {
-        DB db = DBMaker.fileDB("data.db").checksumHeaderBypass().make();
-        //System.out.println("Database names = " + db.getAllNames().toString());
-        //System.out.println("Deleted map = " + db.get("DELETED_MAP"));
-        HTreeMap deletedMap = db.get("infoMap");
-        System.out.println(deletedMap.getKeys());
-        Long fetchedItems = (Long)deletedMap.get("fetchedItems");
-        
+        DB db                          = DBMaker.fileDB("data.db").checksumHeaderBypass().make();
+        Iterable<String> databaseNames = db.getAllNames();
+        System.out.println("Database names = " + db.getAllNames().toString());
+        HTreeMap infoMap = db.get("infoMap");
+
+
+        System.out.println(infoMap.getKeys());
+
+        //Info (mostly stats about the scan
+        Long fetchedItems   = (Long)infoMap.get("fetchedItems");
+        Long failedItems    = (Long)infoMap.get("failedItems");
+        Long newItems       = (Long)infoMap.get("newItems");
+        Long emittedItems   = (Long)infoMap.get("emittedItems");
+        String startLinks   = infoMap.get("startLinks").toString();
+        Long discardedItems = (Long)infoMap.get("discardedItems");
+
+
 
     }
 }
