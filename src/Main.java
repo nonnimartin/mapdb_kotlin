dd8f11c71204dc055235ba91174dfee3b7312154
nonnimartin <nonnimartin@gmail.com>
2020-02-07 15:44:26 -0800

    Now getting the FINISHED URLs list

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 import org.mapdb.*;
 
-import java.lang.reflect.Array;
-import java.util.concurrent.*;
-
 public class Main {
 
     public static void main(String[] args) {
         DB db                          = DBMaker.fileDB("data.db").checksumHeaderBypass().make();
-        Iterable<String> databaseNames = db.getAllNames();
-        System.out.println("Database names = " + db.getAllNames().toString());
-        HTreeMap infoMap = db.get("infoMap");
+        Iterable<String> tableNames    = db.getAllNames();
+        System.out.println("Table names = " + tableNames.toString());
 
+        HTreeMap infoMap              = db.get("infoMap");
+        HTreeMap finishedMap          = db.get("FINISHED_MAP");
+        Iterable<String> finishedUrls = finishedMap.getKeys();
 
-        System.out.println(infoMap.getKeys());
+        for (String thisKey : finishedUrls){
+            System.out.println("this key = " + thisKey);
+        }
+
+        System.out.println("Finished map = " + finishedUrls.toString());
+
+        //System.out.println("Table names = " + db.getAllNames().toString());
+
+        //These all appear to be empty in my own db
+        /*
+        HTreeMap justMap    = db.get("map");
+        HTreeMap errorsMap  = db.get("ERRORS_MAP");
+        HTreeMap deletedMap = db.get("DELETED_MAP");
+         */
 
         //Info (mostly stats about the scan
         Long fetchedItems   = (Long)infoMap.get("fetchedItems");
