d60111bc73e7ded159a9f95fa343c778a535dcb4
nonnimartin <nonnimartin@gmail.com>
2020-02-11 13:21:59 -0800

    Added CLI params

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,48 @@
 import org.mapdb.*;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.ParseException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        CommandLine commandLine;
-        Option crawlDb           = Option.builder("crawldb").argName("crawldb").hasArg().desc("Please add the path to the crawldb data file").build();
-        Options options          = new Options();
+        Options options = new Options();
+        Option input    = new Option("c", "crawldb", true, "Points to the crawldb file you wish to inspect");
+        input.setRequired(true);
+        options.addOption(input);
+
         CommandLineParser parser = new DefaultParser();
-        options.addOption(crawlDb);
-        System.out.println("options = " + options.toString());
+        HelpFormatter formatter  = new HelpFormatter();
+        // initializing the CommandLine obj
+        CommandLine cmd    = null;
+        String crawldbPath = new String();
 
         //handle command line arguments here
         if (args.length > 0) {
+            try {
+                cmd = parser.parse(options, args);
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
 
-//            try {
-//                //commandLine = parser.parse(options);
-//
-//                if (commandLine.hasOption("A")) {
-//                    System.out.print("Option A is present.  The value is: ");
-//                    System.out.println(commandLine.getOptionValue("A"));
-//                }
-//            }catch (ParseException exception) {
-//                System.out.print("Parse error: ");
-//                System.out.println(exception.getMessage());
-//            }
+                System.exit(1);
+            }
+            crawldbPath = cmd.getOptionValue("crawldb");
         }
-
-        DB db                          = DBMaker.fileDB("data.db").checksumHeaderBypass().make();
+        DB db                          = DBMaker.fileDB(crawldbPath).checksumHeaderBypass().make();
         Iterable<String> tableNames    = db.getAllNames();
-        System.out.println("Table names = " + tableNames.toString());
-
         HTreeMap infoMap              = db.get("infoMap");
         HTreeMap finishedMap          = db.get("FINISHED_MAP");
         Iterable<String> finishedUrls = finishedMap.getKeys();
 
+        System.out.println("Finished files:");
         for (String thisKey : finishedUrls){
-            System.out.println("this key = " + thisKey);
+            System.out.println(thisKey);
         }
-
-        System.out.println("Finished map = " + finishedUrls.toString());
 
         //System.out.println("Table names = " + db.getAllNames().toString());
 
